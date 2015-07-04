56ba5ce108e1846ef242a81ea80b0a97b92f3b4c
Phil Evans <pevans@paylocity.com>
2015-07-04 13:01:20 -0400

    Started course

---
+++ Movies.Web/Scripts/movies/movieData.js
@@ -0,0 +1,20 @@
+﻿import {inject} from "aurelia-framework";  
+import {HttpClient} from "aurelia-http-client";
+
+let baseUrl = "/scripts/movies/movies.json";
+
+@inject(HttpClient)
+export class MovieData{
+  
+  constructor(httpClient){
+    this.http = httpClient;
+    
+  }
+
+  getAll(){
+    return this.http.get(baseUrl)
+                    .then(response => {
+                      return response.content;
+                    });
+  }
+}
